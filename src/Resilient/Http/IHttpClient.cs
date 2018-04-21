44a519974e05d3998a256bd618d1dd7ef26ca2af
Rafael do Amaral Silva <rafaelamaralsilva01@gmail.com>
2018-04-21 00:03:24 -0300

    Primeiro commit

---
+++ src/Resilient/Http/IHttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resilient.Http
+{
+    public interface IHttpClient
+    {
+        Task<string> GetStringAsync(string uri, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer);
+
+        Task<HttpResponseMessage> PostAsync<T>(string uri, T item, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer);
+
+        Task<HttpResponseMessage> DeleteAsync(string uri, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer);
+
+        Task<HttpResponseMessage> PutAsync<T>(string uri, T item, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer);
+    }
+}
---
+++ src/Resilient/Http/ResilientHttpClient.cs
@@ -0,0 +1,114 @@
+﻿using Polly;
+using Polly.Wrap;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resilient.Http
+{
+    public class ResilientHttpClient : IHttpClient
+    {
+        private readonly AuthenticationBehaveFactory authenticationBehaveFactory;
+        private ConcurrentDictionary<string, PolicyWrap> policyWrappers;
+
+        public ResilientHttpClient() : this(DefaultFactory)
+        {
+        }
+
+        public ResilientHttpClient(AuthenticationBehaveFactory authenticationBehaveFactory)
+        {
+            this.authenticationBehaveFactory = authenticationBehaveFactory;
+            this.policyWrappers = new ConcurrentDictionary<string, PolicyWrap>();
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(string uri, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetStringAsync(string uri, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string uri, T item, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync<T>(string uri, T item, AuthenticationMethodTypes authorizationMethod = AuthenticationMethodTypes.Bearer)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<T> HttpInvoker<T>(string origin, Func<Task<T>> action)
+        {
+            var normalizedOrigin = NormalizeOrigin(origin);
+
+            //if (!_policyWrappers.TryGetValue(normalizedOrigin, out PolicyWrap policyWrap))
+            //{
+            //    policyWrap = Policy.WrapAsync(_policyCreator(normalizedOrigin).ToArray());
+            //    _policyWrappers.TryAdd(normalizedOrigin, policyWrap);
+            //}
+
+            // Executes the action applying all 
+            // the policies defined in the wrapper
+            return await policyWrap.ExecuteAsync(action, new Context(normalizedOrigin));
+        }
+
+        private static string NormalizeOrigin(string origin)
+        {
+            return origin?.Trim()?.ToLower();
+        }
+
+        private static string GetOriginFromUri(string uri)
+        {
+            var url = new Uri(uri);
+
+            var origin = $"{url.Scheme}://{url.DnsSafeHost}:{url.Port}";
+
+            return origin;
+        }
+
+        private static IAuthenticationBehave DefaultFactory(AuthenticationMethodTypes type)
+        {
+            switch (type)
+            {
+                case AuthenticationMethodTypes.Bearer:
+                    return new BearerAuthenticateBehave();
+                case AuthenticationMethodTypes.OpenId:
+                    return null;
+                default:
+                    throw new NotSupportedException(type.ToString());
+            }
+        }
+    }
+
+    public enum AuthenticationMethodTypes
+    {
+        Bearer = 1,
+        OpenId = 2,
+    }
+
+    public delegate IAuthenticationBehave AuthenticationBehaveFactory(AuthenticationMethodTypes authenticationMethodType);
+
+    public interface IAuthenticationBehave
+    {
+        void Prepare(HttpRequestMessage request);
+    }
+
+    public interface IBearerAuthenticateBehave : IAuthenticationBehave
+    {
+    }
+
+    public class BearerAuthenticateBehave : IBearerAuthenticateBehave
+    {
+        public void Prepare(HttpRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

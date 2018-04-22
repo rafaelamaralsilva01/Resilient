25daa7beaa0ad616c5ec0083d2ec57e4eb0a610b
Rafael do Amaral Silva <rafaelamaralsilva01@gmail.com>
2018-04-21 23:31:23 -0300

    Adiciona o projeto de testes

---
+++ src/Resilient.Tests/ResilientClientTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Resilient.Tests
+{
+    [TestClass]
+    public class ResilientClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

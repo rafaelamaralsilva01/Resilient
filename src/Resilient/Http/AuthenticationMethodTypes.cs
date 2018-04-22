1fc6138f1bdb1ce2f557bd06b645afca3dc5b763
Rafael do Amaral Silva <rafaelamaralsilva01@gmail.com>
2018-04-21 23:23:29 -0300

    Corrigindo .git ignore.

---
+++ src/Resilient/Http/AuthenticationMethodTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resilient.Http
+{
+    public enum AuthenticationMethodTypes
+    {
+        Bearer = 1,
+        OpenId = 2,
+    }
+}
